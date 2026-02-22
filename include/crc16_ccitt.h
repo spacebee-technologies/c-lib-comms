4fae6550e9a3fad32952eaeba33dad46d3d6b70d
Lautaro Mendieta <lemendieta96@gmail.com>
2026-02-22 09:45:32 -0300

    Update documentation for CRC calculation function

--- include/crc16_ccitt.h
+++ include/crc16_ccitt.h
@@ -4,7 +4,14 @@
 #include <stddef.h>
 #include <stdint.h>
 
-// CRC-16/CCITT-FALSE: poly=0x1021 init=0xFFFF refin=false refout=false xorout=0x0000
+/**
+ * @brief Calculate **CRC-16/CCITT-FALSE**: poly=0x1021 init=0xFFFF
+ *        refin=false refout=false xorout=0x0000
+ *
+ * @param data Data over which the CRC will be calculated
+ * @param len Size of the buffer
+ * @return uint16_t CRC calculated value
+ */
 uint16_t crc16_ccitt_false(const uint8_t *data, size_t len);
 
 #endif  // CRC16_CCITT_H_
